7010f9949ed8684ef9740f55e32265ae836cce0a
pburgov <pburgov@gmail.com>
2019-09-11 07:08:04 +0200

    Adding UnitOfMeasure

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,5 +1,7 @@
 package guru.springframework.domain;
 
+import java.util.Set;
+
 import javax.persistence.*;
 
 /**
@@ -22,6 +24,8 @@ public class Recipe {
     //todo add
     //private Difficulty difficulty;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set <Ingredient> ingredients;
     @Lob
     private Byte[] image;
 
